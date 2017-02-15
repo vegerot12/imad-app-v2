4c2f6376b7ed3a15eed0a7a2625d48609a128e83
vegerot12 <nandhinirajavell@gmail.com>
2017-02-15 21:26:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter + 1;
+  res.send(counter,ToString());
+});
 
 app.get('/ui/article-one', function (req, res) {
    res.send(createTemplate(articleone)); 
